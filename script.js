16a6d70780f39dce6be204f84c569eb3c071e118
Tornike-max <84152983+Tornike-max@users.noreply.github.com>
2023-04-23 03:23:52 +0400

    use leaflet

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 
 let form = document.querySelector('.form');
+let inputDistance = document.querySelector('.dis')
 let map,mapEvent;
 // navigator.geolocation.getCurrentPosition ასე ვიძახებთ ახლანდელ პოზიციას და getCurrentPosition-ს გადაეცემა ორი ფუნქცია
 // რომლებიც მოქმედებენ შემდეგ ნაირად, თუ ახლანდელ ადგილს იპოვნის უპრობლეოდ მაშინ შევდა პირველ ფუნქციაში, სადაც ჩვენ გვაქვს
@@ -17,8 +18,8 @@ navigator.geolocation.getCurrentPosition(
     console.log(curLoc);
     const coords = [latitude, longitude]
 
-    map = L.map("map").setView(coords, 13);
-    console.log(map)
+    map = L.map("map").setView(coords, 14);
+  
 
     L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
       attribution:
@@ -28,6 +29,7 @@ navigator.geolocation.getCurrentPosition(
     map.on('click', function(mapE){
         mapEvent = mapE;
         form.classList.remove('hidden')
+        inputDistance.focus()
     })
   },
   function () {
@@ -35,22 +37,22 @@ navigator.geolocation.getCurrentPosition(
   }
 );
 
-form.addEventListener('submit', function(e){
-    e.preventDefault()
-        let { lat, lng } = mapEvent.latlng
-        L.marker([lat,lng])
-        .addTo(map)
-        .bindPopup(
-            L.popup({
-                maxWidth: 250,
-                minWidth:50,
-                autoClose:false,
-                closeOnClick:false,
-                className:'running-popup'
-            })
-        )
-        .setPopupContent('Current location')
-        .openPopup();
+form.addEventListener('submit', function(){
+  console.log('dsa')
+  let { lat, lng } = mapEvent.latlng
+  L.marker([lat,lng])
+  .addTo(map)
+  .bindPopup(
+      L.popup({
+          maxWidth: 250,
+          minWidth:50,
+          autoClose:false,
+          closeOnClick:false,
+          className:'running-popup'
+      })
+  )
+  .setPopupContent('Current location')
+  .openPopup();
 })
 
 
